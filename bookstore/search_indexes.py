8c7ff4fe2d2bdaa5c6a34a4e4e678c57a969d5d1
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-07-09 11:37:25 +0200

    Adding search index definition for haystack

---
+++ bookstore/search_indexes.py
@@ -0,0 +1,16 @@
+from haystack import indexes
+
+from bookstore.models import Author, Book
+
+class BookIndex(indexes.SearchIndex, indexes.Indexable):
+	text = indexes.CharFiled(document = True, use_template=True)
+	author = indexes.CharFiled(model_attr = 'author')
+
+	def get_model(self):
+		return Book
+	
+	def index_queryset(self, using = None):
+		"""
+			Used when the entire index for model is updated.
+		"""
+		return self.get_model().objects.all()

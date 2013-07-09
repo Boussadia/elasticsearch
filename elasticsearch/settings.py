6ec169b0d8f1f8e4e3e4c92d1ae2b08d1d662203
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-07-09 11:14:19 +0200

    Addind haystack configuration

--- elasticsearch/settings.py
+++ elasticsearch/settings.py
@@ -162,3 +162,13 @@ LOGGING = {
         },
     }
 }
+
+
+# Haystack configuration
+HAYSTACK_CONNECTIONS = {
+	'default': {
+		'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+		'URL': 'http://127.0.0.1:9200/',
+		'INDEX_NAME': 'haystack',
+	},
+}

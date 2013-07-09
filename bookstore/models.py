d4e41d9e0c0af7eab0a12ef6fc9e5495dc8cee91
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-07-09 11:05:44 +0200

    Adding author and book models in bookstore app

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+	first_name = models.CharField(max_length = 100)
+	last_name = models.CharField(max_length = 100)
+
+	def __unicode__(slef):
+		return '%s %s'%(self.first_name, self.last_name)
+
+class Book(models.Model):
+	author = models.ForeignKey(Author)
+	title = models.CharField(max_length = 100)
+
+	def __unicode__(self):
+		return '%s by %s'%(self.title, self.author)
